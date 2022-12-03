8fbdd4fa1adc9b2536cdbeb98776f6512254edd8
nina <nina.stewens@stud.tu-darmstadt.de>
2022-12-03 15:16:48 +0100

    changed converter

--- json_converter.py
+++ json_converter.py
@@ -1,5 +1,5 @@
 
-#import py2cfg.py2cfg.builder as builder
+import py2cfg.py2cfg.builder as builder
 import json
 import os
 import code_prediction_transformer_master.parse_python as parse_python
@@ -15,17 +15,16 @@ name = "ninatest"
 with open(f'{name}.json', 'w') as fjson, open('errorlog.txt', 'w') as errorlog:
          for root, _, files in os.walk(absolute_path):
              for file in files:
-                # with open(os.path.join(root, file), "r") as fpy:
-                 print(parse_python.parse_file(os.path.join(root, file)))
-#                     try:
-#                         cfg, mylist = CFGBuilder().build_from_file(name, fpy.name) #have second output of json info as dict
-#                         if mylist is None:
-#                             print("ERROR: could not create entry: ", fpy.name)
-#                         else:
-#                             json_object= json.dumps(mylist, indent=4)
-#                             fjson.write(json_object)
-#                     except Exception as e:
-#                         errorlog.write(fpy.name + " " + "\n")
+                with open(os.path.join(root, file), "r") as fpy:
+                    try:
+                        cfg, mylist = builder.CFGBuilder().build_from_file(name, fpy.name) #have second output of json info as dict
+                        if mylist is None:
+                            print("ERROR: could not create entry: ", fpy.name)
+                        else:
+                            json_object= json.dumps(mylist, indent=4)
+                            fjson.write(json_object)
+                    except Exception as e:
+                        errorlog.write(fpy.name + " " + str(e) + fpy.errors + "\n")
 
 
 #print(mylist)
