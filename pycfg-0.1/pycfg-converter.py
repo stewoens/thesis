90c0f0e625439567485241cd33122c272159a0ee
nina <nina.stewens@stud.tu-darmstadt.de>
2022-12-12 15:52:48 +0100

    started new pycfg attempt

--- pycfg-0.1/pycfg-converter.py
+++ pycfg-0.1/pycfg-converter.py
@@ -1,1 +1,23 @@
 import pycfg.pycfg as pycfg
+
+import json
+import sys
+
+# filepath = r"C:/Users/ninas/OneDrive/Documents/UNI/Productive-Bachelors/DATA/data3"
+filepath = r"C:/Users/ninas/OneDrive/Documents/UNI/Productive-Bachelors/py2cfg/examples/else_02.py"
+
+cfg,first,last = pycfg.get_cfg(filepath)
+for i in sorted(cfg.keys()):
+    print(i,'parents:', cfg[i]['parents'], 'children:', cfg[i]['children'])
+
+
+ #incase dots is on ...whatever that means
+# arcs = []
+# cfg = pycfg.PyCFG()
+# cfg.gen_cfg(pycfg.slurp(filepath).strip())
+# g = pycfg.CFGNode.to_graph(arcs)
+# g.draw(args.pythonfile + '.png', prog='dot')
+# print(g.string(), file=sys.stderr)
+
+
+
--- pycfg-0.1/pycfg/pycfg.py
+++ pycfg-0.1/pycfg/pycfg.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 # Author: Rahul Gopinath <rahul.gopinath@cispa.saarland>
 # License: GPLv3
 """
