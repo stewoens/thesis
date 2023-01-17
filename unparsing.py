180e2480deaf9d5231ad83a3810112c49d980c90
nina <nina.stewens@stud.tu-darmstadt.de>
2023-01-17 11:47:54 +0100

    unparsing update (wrong because dp is list != ast)

--- unparsing.py
+++ unparsing.py
@@ -1,24 +1,32 @@
-from tqdm import tqdm
+#from tqdm import tqdm
 import ast
+import json
 import os
 
-def file_tqdm(f):
-    return tqdm(f, total=get_number_of_lines(f))
+# def file_tqdm(f):
+#     return tqdm(f, total=get_number_of_lines(f))
 
 def create_filename(root, index):
-    x  = root +"/file" + index
-    print(x)
+    x  = root +"/file_" + str(index)
+    return x 
 
 data = r"C:/Users/ninas/OneDrive/Documents/UNI/Productive-Bachelors/DATA/py150/python50k_eval.json"
-root = r"OUTPUT/unparsed-data"
-tmp = 0
+root = r"C:/Users/ninas/OneDrive/Documents/UNI/Productive-Bachelors/OUTPUT/unparsed-data"
+
 
 def unparse_data():
+    tmp = 0
     with open(data, "r") as f:
-        output = create_filename(root,tmp)
-        with open(output, "w") as fout:
-            for line in file_tqdm(f):
+        for line in f:
+            output = create_filename(root,tmp)
+            tmp =tmp +1
+            with open(output, "w") as fout:
                 dp = json.loads(line.strip())
+                print(dp.type())
+                dp.unpa
+                # fout.write(ast.unparse(dp))
+                if tmp >= 1: return
+    
             
             
-create_filename(root,tmp)
+unparse_data()
