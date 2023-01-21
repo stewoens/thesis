5312a8409d063ddedfafb907c9f16775b84f1b8a
nina <nina.stewens@stud.tu-darmstadt.de>
2023-01-21 18:56:19 +0100

    data parser imprived for testing

--- data_parser.py
+++ data_parser.py
@@ -3,8 +3,9 @@ from cptm.ninacode.parse_cfg import parse_file_2cfg
 import os
 
 data = r"C:/Users/ninas/OneDrive/Documents/UNI/Productive-Bachelors/DATA/data2"
-test = r"C:/Users/ninas/OneDrive/Documents/UNI/Productive-Bachelors/DATA/data3/00/wikihouse/asset.py"
-output ='OUTPUT/my_dataset'
+test = r"C:/Users/ninas/OneDrive/Documents/UNI/Productive-Bachelors/test_file.py"
+output_cfg ='OUTPUT/my_dataset_cfg'
+output_ast ='OUTPUT/my_dataset_ast'
 cfg_data = 'OUTPUT/my_cfg'
 
 
@@ -22,12 +23,17 @@ def test_path_prep():
             else: return
 
  
-def test_output():           
-    with open(output,'w') as out, open(test, 'r')as f, open(test2, 'r')as g:
+def test_output_cfg():           
+    with open(output_cfg,'w') as out, open(test, 'r')as f:
+        print >>out, parse_file_2cfg(test)
+        print "finished :)"
+        
+def test_output_ast():           
+    with open(output_ast,'w') as out, open(test, 'r')as f:
         print >>out, parse_file(test)
-        print >>out, parse_file(test2)
+        print "finished :)"
 
-def data_parser():
+def data_parser_cfg():
     i =0
     with open(output,'w') as out:
         for root, _, files in os.walk(data):
@@ -45,7 +51,7 @@ def data_parser():
     print "finished :)"
     return
 
-data_parser()
+test_output_ast()
 
 
 
---
+++ test_file.py
@@ -0,0 +1,11 @@
+import os as os
+global x
+
+class BitchesBeProductive(object):
+    def functions_for_baddies(list):
+        for object in list:
+            print object 
+            x = 7 +10
+        
+        
+    
