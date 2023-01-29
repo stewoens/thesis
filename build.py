31781f7ac1e61a61816fa6ef8172a9462b126899
nina <nina.stewens@stud.tu-darmstadt.de>
2023-01-29 13:01:52 +0100

    initiating link (unfinished)

--- build.py
+++ build.py
@@ -2,11 +2,72 @@
 Control flow graph builder.
 """
 import ast
+import astor
+
 test = r"C:/Users/ninas/OneDrive/Documents/UNI/Productive-Bachelors/example.py"
 
 stmnt_types = ['Module' , 'If', 'For', 'While', 'Break', 'Continue', 'ExceptHandler', 'With']
 
+class Link(object):
+    """
+    Link between blocks in a control flow graph.
 
+    Represents a control flow jump between two blocks. Contains an exitcase in
+    the form of an expression, representing the case in which the associated
+    control jump is made.
+    """
+
+    __slots__ = (
+        "source",
+        "target",
+        "exitcase",
+        "highlight",
+    )
+
+    def __init__(
+        self,
+        source,
+        target,
+        exitcase = None, #ast- Compare
+    ):
+        assert isinstance(target, Block), "Source of a link must be a block"
+        assert isinstance(target, Block), "Target of a link must be a block"
+        # Block from which the control flow jump was made.
+        self.source = source
+        # Target block of the control flow jump.
+        self.target = target
+        # 'Case' leading to a control flow jump through this link.
+        self.exitcase = exitcase
+
+    def __str__(self):
+        return "link from {0} to {1}".format(self.source,self.target)
+
+    def __repr__(self):
+        # This isn't how repr is supposed to be used... We should be able to
+        # deep copy this object by calling eval(repr(link))`.
+        if self.exitcase is not None:
+            return "{self}, with exitcase {ast.dump(self.exitcase)}"
+        return str(self)
+
+    def jumpfrom(self):
+        """Return the line of source end"""
+        return self.source.end()
+
+    def jumpto(self):
+        """Return the line of target start"""
+        return self.target.at()
+
+    def get_exitcase(self): #??? what is an exitcase?
+        """
+        Get a string containing the Python source code corresponding to the
+        exitcase of the Link.
+
+        Returns:
+            A string containing the source code.
+        """
+        if self.exitcase:
+            return astor.to_source(self.exitcase)
+        return ""
 
 def read_file_to_string(filename):
     f = open(filename, 'rt')
@@ -25,6 +86,61 @@ class CFGBlock():
         dict = {"id":id, "type":typ,"content":[],"children":[],"parents":[]}
         self.d= dict
         
+    def __init__(self, id,type):
+        # Id of the block.
+        self.id = id
+        # Statements in the block.
+        self.content = []
+        # type of the block
+        self.type =type
+        # Calls to functions inside the block (represents context switches to
+        # some functions' CFGs).???
+        self.func_calls = []
+        # Links to predecessors in a control flow graph.
+        self.predecessors= []
+        # Links to the next blocks in a control flow graph. 
+        self.exits = []
+        # Function blocks within this block ???
+        self.func_blocks = []
+        
+    def at(self):
+        """
+        Get the line number of the first statement of the block in the program.
+        """
+        if self.content and self.content[0].lineno >= 0:
+            return self.content[0].lineno
+        return -1
+
+    def end(self):
+        """
+        Get the line number of the last statement of the block in the program.
+        """
+        if self.content and self.content[-1].lineno >= 0:
+            return self.content[-1].lineno
+        return -1     
+
+    def is_empty(self):
+        """
+        Check if the block is empty.
+
+        Returns:
+            A boolean indicating if the block is empty (True) or not (False).
+        """
+        return not self.content
+
+    def add_content(self, node):
+        """
+        Ive made node be dump(node)so far but maybe can be node as well?
+        """
+        self.content.append(node)
+    
+    #not sure how the exits work yet
+    def add_exit(self, next, exitcase=None):
+        link = Link(self, next, exitcase)
+        self.exits.append(link)
+        next.predecessors.append(link)
+
+
 
 class CFGBuilder():
     """
@@ -56,19 +172,6 @@ class CFGBuilder():
             
             return block
      
-        
-    def add_statement(self, block, content):
-        """
-        Add a content to a block.
-
-        Args:
-            block: A CFGBlock object to which a content must be added.
-            content: An AST node representing the content that must be
-                       added to the current block.
-                       OR A STRING?
-        """
-        block.d["content"].append(content)
-    
     
     def add_child(self, block, child):
         if child not in block.d["children"]:
@@ -130,7 +233,7 @@ class CFGBuilder():
                 self.add_child(current_block, self.traverse(child))
                 
         elif isinstance(node, ast.If):
-            if self.current_block.statements:
+            if self.current_block.content:
                 # Add the If statement at the beginning of the new block.
                 cond_block = self.new_block()
                 self.add_statement(cond_block, node)
