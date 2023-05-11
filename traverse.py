528759163ee5b31d44988a74b8bab0ea4634645f
nina <nina.stewens@stud.tu-darmstadt.de>
2023-05-11 11:31:43 +0100

    astprint works

--- traverse.py
+++ traverse.py
@@ -1,10 +1,23 @@
 import json
+import ast
+import astunparse
+from astprint import as_tree, as_code
 from cptm.utils import file_tqdm, get_dfs, separate_dps
 
 data = 'OUTPUT/sample_data.json'
 out = 'OUTPUT/dps.txt'
 
-with open(data, "r") as f, open(out, "w") as fout:
-    for line in file_tqdm(f):
-        dp = json.loads(line.strip())
-        asts = separate_dps(dp, 1000)
+
+def traverse():
+    with open(data, "r") as f, open(out, "w") as fout:
+        for line in file_tqdm(f):
+            # dp = json.loads(line.strip())
+            # asts = separate_dps(dp, 1000)
+            dp = ast.literal_eval(line)
+            for cfg in dp:
+                #each cfg will generate a different sequence
+                for block in cfg:
+                    for s in block['statements']:
+                        
+
+traverse()
