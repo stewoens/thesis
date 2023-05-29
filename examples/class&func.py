28fa86b46d5745e894aa60c7f859699a24e5b4c2
nina <nina.stewens@stud.tu-darmstadt.de>
2023-05-29 17:51:03 +0100

    examples

---
+++ examples/class&func.py
@@ -0,0 +1,15 @@
+class new_class():
+    def __init__(self, id, value):
+        self.id = id
+        self.value =value
+
+    def func():
+        for i in x:
+            if i == 3:
+                print "three"
+            else:
+                print i
+    
+    print "woohoo class"
+    x = 7 
+    y =8
---
+++ examples/general01.py
@@ -0,0 +1,16 @@
+x = [0,1,2,3]
+
+for i in x:
+    if i == 3:
+        print "three"
+    else:
+        print i
+
+def func():
+    for i in x:
+        if i == 3:
+            print "three"
+        else:
+            print i
+
+func()
